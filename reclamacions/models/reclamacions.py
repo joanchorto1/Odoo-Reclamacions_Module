38bcf686d8f6ca9429228f34cbd242588e14f1b7
alumne <joanchorto1@iesebre.com>
2024-03-19 15:57:56 +0100

    Estat actual 19/03

--- reclamacions/models/reclamacions.py
+++ reclamacions/models/reclamacions.py
@@ -15,7 +15,7 @@ class Reclamacion(models.Model):
     sale_order_id = fields.Many2one('sale.order', string='Comanda de venta')
     message_ids = fields.One2many('reclamacion.mensaje', 'reclamacion_id', string='Mensajes')
     invoice_count = fields.Integer(compute='_compute_invoice_count', string='Número de facturas')
-    delivery_count = fields.Integer(compute='_compute_delivery_count', string='Número de envíos')
+    # delivery_count = fields.Integer(compute='_compute_delivery_count', string='Número de envíos')
     state = fields.Selection([
         ('nueva', 'Nueva'),
         ('en_proceso', 'En Proceso'),
@@ -28,6 +28,14 @@ class Reclamacion(models.Model):
     def _compute_invoice_count(self):
         for rec in self:
             rec.invoice_count = len(rec.sale_order_id.invoice_ids)
+    
+    def button_cancel_sale_order(self):
+        for rec in self:
+            sale_order = rec.sale_order_id
+            if sale_order.state not in ['cancel', 'done']:
+                sale_order.action_cancel()
+                for line in sale_order.order_line:
+                    line.product_id.qty_available += line.product_uom_qty
 
     # @api.depends('sale_order_id.picking_ids')
     # def _compute_delivery_count(self):
